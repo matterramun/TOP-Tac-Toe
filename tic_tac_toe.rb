3bc2064b4edc9e1fc19181d1b233404288a02452
matterramun <matterramun@gmail.com>
2023-02-09 21:23:26 -0500

    Move board display to own method. Add marker position gathering.
    - Move display of board to its own method.
    - Implement an array.each loop for board display to show nicely.
    - change board variable from a hash to array, because order did matter!
    - Add turn announcement.
    - Add gathering of which position the user wants to place their piece.
    - Add error handling to marker position gathering.
    - Removed calling of Class methods and added them to initialize method.
    - Add board display between turns
    - Make "ROUND" announcement actually return round
    
    To Do:
    + Add placement of marker
    + Add handling to prevent player from overwriting an existing marker
    + Add victory checker, add to place marker method
    ++ Add handling for colloqial terms of rows and columns (top, middle bottom, etc)
    
    Future me, make more atomic commits

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,13 +6,23 @@ class Game
   @@marker2 = 'O'
 
   def initialize
-    @board = { 'top' => %w[_ _ _], 'mid' => %w[_ _ _], 'bot' => %w[_ _ _] }
-    p @board['top'].to_s
-    p @board['mid'].to_s
-    p @board['bot'].to_s
+    @board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
+    showboard
+    namereq
+    play
   end
 
-  def self.namereq
+  def showboard
+    @board.each_with_index do |_a, row_index|
+      @board[row_index].each_with_index do |mark, col_index|
+        print mark
+        print '|' if col_index < 2
+      end
+      puts ''
+    end
+  end
+
+  def namereq
     puts 'Player 1, enter your name:'
     @name1 = gets.chomp
     @name1 = @name1.to_s.empty? ? 'Player 1' : @name1
@@ -24,23 +34,43 @@ class Game
   # TODO: victory checker... look for victory condition
 
   # TODO: placemarker method
-  def self.placemarker
-    p "Player turn #{@turn}"
+  def placemarker
+    currentplayer = @turn == 1 ? @name1 : @name2
+    currentmarker = @turn == 1 ? @@marker1 : @@marker2
+    p "Its your turn, #{currentplayer}!"
+
+    puts "#{currentplayer} which row?"
+    row = gets.chomp.to_i
+    while row > 3 || row.zero?
+      puts "Try again #{currentplayer}, pick a number between 1 and 3"
+      row = gets.chomp.to_i
+    end
+
+    puts "#{currentplayer} which column?"
+    column = gets.chomp.to_i
+    while column > 3 || column.zero?
+      puts "Try again #{currentplayer}, pick a number between 1 and 3"
+      column = gets.chomp.to_i
+    end
   end
 
-  # DONE: turn iteration...
-  def self.play
+  def play
     @turn = 1
-    i = 0
-    while i <= 9
+    @i = 1
+    while @i <= 9
+      if (@i + 1).even?
+        puts ''
+        puts "ROUND #{(@i + 1) / 2}"
+      elsif @i == 1
+        puts 'ROUND 1'
+      end
+      showboard
       placemarker
-      @turn.zero? ? @turn += 1 : @turn -= 1
-      i += 1
-      p @turn
+      @turn == 1 ? @turn += 1 : @turn -= 1
+      @i += 1
     end
+    puts 'CAT\'S GAME'
   end
 end
 
 Game.new
-Game.namereq
-Game.play
